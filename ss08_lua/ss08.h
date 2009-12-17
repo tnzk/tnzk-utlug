0d761aefb6c32182e8780404807be01b4e06f95b
tnzk <tnzk@juliet.(none)>
2009-12-17 17:01:45 +0900

    Moving (go forward or backward, turn left or right)

--- ss08_lua/ss08.h
+++ ss08_lua/ss08.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 typedef struct {
   int id;
   int type;
@@ -6,4 +8,8 @@ typedef struct {
   double theta;
 } TkbEnemy;
 
-static const int NUM_ENEMY = 5;
+static const int NUM_ENEMY = 2;
+static const double D_OMG = M_PI / 32.0;
+
+#define RAD2DEG(rad)   (   (rad) * 180.0 / M_PI)
+#define DEG2RAD(theta) ( (theta) / 180.0 * M_PI )
--- ss08_lua/ss08_render.c
+++ ss08_lua/ss08_render.c
@@ -11,6 +11,7 @@
 // Using C standard libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "ss08.h"
 
@@ -32,9 +33,15 @@ void reshape( int w, int h)
   glMatrixMode( GL_MODELVIEW);
 }
 
-void showbox( int size)
+void showbox( int size, int type)
 {
   int s = size >> 1;
+  int m = size >> 3;
+
+  glColor3f( 0.8 * ((type & 1) >> 0),
+	     0.8 * ((type & 2) >> 1),
+	     0.8 * ((type & 4) >> 2));
+
 
   glBegin(GL_TRIANGLES);
   
@@ -47,6 +54,21 @@ void showbox( int size)
   glTexCoord2f(1 , 0); glVertex2d(  s, -s);
   
   glEnd();
+
+  glColor3f( 1.0, 1.0, 1.0); 
+  glBegin(GL_TRIANGLES);
+  
+  glTexCoord2f(0 , 0); glVertex2d( -m + 10, -m);
+  glTexCoord2f(1 , 1); glVertex2d(  m + 10,  m);
+  glTexCoord2f(0 , 1); glVertex2d( -m + 10,  m);
+ 
+  glTexCoord2f(0 , 0); glVertex2d( -m + 10, -m);
+  glTexCoord2f(1 , 1); glVertex2d(  m + 10,  m);
+  glTexCoord2f(1 , 0); glVertex2d(  m + 10, -m);
+  
+  glEnd();
+
+
 }
 
 void display()
@@ -62,11 +84,8 @@ void display()
       glTranslatef( enms[i].x,
 		    enms[i].y,
 		    0);
-      glRotatef( enms[i].theta, 0, 0, 1.0);
-      glColor3f( 0.8 * ((enms[i].type & 1) >> 0),
-		 0.8 * ((enms[i].type & 2) >> 1),
-		 0.8 * ((enms[i].type & 4) >> 2));
-      showbox( 32);
+      glRotatef( RAD2DEG(enms[i].theta), 0, 0, 1.0);
+      showbox( 32, enms[i].type);
     }
     glPopMatrix;
   }
@@ -99,6 +118,26 @@ void get_decision(lua_State* L, int* mov, int* dir, int* sht)
   lua_pop(L, 1);
 }
 
+void enm_move( TkbEnemy* enm, int mov)
+{
+  double dx = mov * 4.0 * cos(enm->theta);
+  double dy = mov * 4.0 * sin(enm->theta);
+
+  enm->x += (int)dx;
+  enm->y += (int)dy;
+
+}
+
+void enm_turn( TkbEnemy* enm, int dir)
+{
+
+  switch(dir){
+  case -1: enm->theta -= D_OMG; break;
+  case  0: break;
+  case  1: enm->theta += D_OMG; break;
+  }
+}
+
 void timer(int value) {
   int i;
   int mov = 0; // move
@@ -107,14 +146,10 @@ void timer(int value) {
 
   for( i = 0; i < NUM_ENEMY; i++){
     lua_State* L = Ls[enms[i].type];
-
     get_decision(L, &mov, &dir, &sht);
-    
-    switch(dir){
-    case -1: enms[i].theta -= 1.0; break;
-    case  0: break;
-    case  1: enms[i].theta += 1.0; break;
-    }
+    enm_move( enms + i, mov);
+    enm_turn( enms + i, dir);
+
   }
 
   glutPostRedisplay();
